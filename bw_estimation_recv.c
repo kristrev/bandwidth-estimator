19dfa99f507af108cf067af7b3efa20e16d3e079
Kristian Evensen <kristrev@armstrong.(none)>
2012-12-23 00:32:56 +0100

    Receiver binds to local socket

--- bw_estimation_recv.c
+++ bw_estimation_recv.c
@@ -2,6 +2,10 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
 
 #include "bw_estimation_packets.h"
 
@@ -11,6 +15,53 @@
 //If no reply is received 60 seconds after last reply, exit with failure
 //A similar approach is taken for the sender when it comes to END_SESSION
 
+//Bind the local socket. Should work with both IPv4 and IPv6
+int bind_local(char *local_addr, char *local_port, int socktype){
+  struct addrinfo hints, *info, *p;
+  int sockfd;
+  int rv;
+  int yes=1;
+  
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = socktype;
+  
+  if((rv = getaddrinfo(local_addr, local_port, &hints, &info)) != 0){
+    fprintf(stderr, "Getaddrinfo (local) failed: %s\n", gai_strerror(rv));
+    return -1;
+  }
+  
+  for(p = info; p != NULL; p = p->ai_next){
+    if((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+      perror("Socket:");
+      continue;
+    }
+
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+      close(sockfd);
+      perror("Setsockopt");
+      continue;
+    }
+
+    if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1){
+      close(sockfd);
+      perror("Bind (local)");
+      continue;
+    }
+
+    break;
+  }
+
+  if(p == NULL){
+    fprintf(stderr, "Local bind failed\n");
+    freeaddrinfo(info);  
+    return -1;
+  }
+
+  freeaddrinfo(info);
+
+  return sockfd;
+}
 void usage(){
     fprintf(stderr, "All command line arguments are required\n");
     fprintf(stderr, "-b : Bandwidth (in Mbit/s, only integers)\n");
@@ -24,7 +75,7 @@ void usage(){
 int main(int argc, char *argv[]){
     uint16_t bandwidth = 0, duration = 0, payloadLen = 0;
     uint8_t *srcIp = NULL, *senderIp = NULL, *senderPort = NULL;
-    int32_t c;
+    int32_t c, udpSockFd = -1;
 
     //Arguments + values
     if(argc != 13){
@@ -65,6 +116,10 @@ int main(int argc, char *argv[]){
     }
 
     //Bind UDP socket
+    if((udpSockFd = bind_local(srcIp, NULL, SOCK_DGRAM)) == -1){
+        fprintf(stderr, "Binding to local IP failed\n");
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 }
