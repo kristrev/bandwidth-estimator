c723dce987a1e7227cedc87668b0689e8aa7de96
Kristian Evensen <kristrev@armstrong.(none)>
2012-12-23 00:25:12 +0100

    Added current idea for packets

---
+++ bw_estimation_packets.h
@@ -0,0 +1,29 @@
+#include <stdint.h>
+
+typedef enum{
+    NEW_SESSION=0, //The sender should start a new send session
+    DATA, //Pkt shall be used for measurement
+    END_SESSION, //Sent from sender after last data packet
+} pktType;
+
+#define BW_MANDATORY \
+    uint8_t type;
+
+//Will I need a timestamp? All NEW_SESSION packets have same src/port, so I can just check
+//for that
+struct newSessionPkt{
+    BW_MANDATORY;
+    uint16_t duration;
+    uint16_t bw; //Bw in Mbit/s, only allow integers for now
+    uint16_t payload_len;
+};
+
+struct dataPkt{
+    BW_MANDATORY;
+    uint8_t *buf; //Can't use a constant as buffer size, it is set at runtime
+};
+
+//Not sure if I need more here
+struct endSessionPkt{
+    BW_MANDATORY;  
+};
