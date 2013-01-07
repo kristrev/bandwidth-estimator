833a3f0a188be0ddf7c947e87309f250ad312abe
Kristian Evensen <kristrev@simula.no>
2013-01-07 17:08:15 +0100

    Cleaned up parse script, but there is still a bug in processing of files

--- plot_recv_times.py
+++ plot_recv_times.py
@@ -1,29 +1,6 @@
 import sys
 import os
 
-#Finds all packets that were received before interval_end, returns -1 if EOF
-def readPkts(logfile, interval_end):
-    interval_bytes = 0 
-
-    while True:
-        line = logfile.readline()
-        line_len = len(line)
-        line = line.strip().split(' ')
-
-        if line[0] == '':
-            return -1
-        
-        tstamp, numbytes = float(line[0]), int(line[1])
-
-        if tstamp >= interval_end:
-            logfile.seek(-1*line_len, os.SEEK_CUR)
-            return interval_bytes
-        else:
-            interval_bytes += numbytes
-        
-    return interval_bytes
-
-
 #What interval should be used on x-axis. I.e., how many ms should one tick
 #express
 interval_length = int(sys.argv[1])
@@ -34,19 +11,41 @@ line = logfile.readline().strip().split(' ')
 initial_tstamp = float(line[0])
 
 cur_interval_start = initial_tstamp
+
+#Timestamp in file is in seconds
 cur_interval_end = initial_tstamp + (interval_length / 1000.0)
 
 #Reset file descriptor
 logfile.seek(0, os.SEEK_SET)
 x_val = 0
+read = True
+interval_bytes = 0
 
 while True:
-    interval_bytes = readPkts(logfile, cur_interval_end)
+    #To avoid the additional function, have a check here for read. This is set
+    #to false when a new interval is started
+    if read:
+        line = logfile.readline()
 
-    if interval_bytes == -1:
-        break
+        if line == '':
+            break
 
-    cur_interval_start = cur_interval_end
-    cur_interval_end = cur_interval_start + (interval_length / 1000.0)
-    print x_val, interval_bytes
-    x_val += 1
+        line = line.strip().split(' ')
+    
+    tstamp, numbytes = float(line[0]), int(line[1])
+
+    if tstamp > cur_interval_end:
+        print cur_interval_start, cur_interval_end
+
+        print x_val, interval_bytes
+        x_val += 1
+        interval_bytes = 0
+
+        read = False
+        cur_interval_start = cur_interval_end
+        cur_interval_end = cur_interval_start + (interval_length / 1000.0)
+    else:
+        read = True
+        interval_bytes += numbytes
+
+logfile.close()
